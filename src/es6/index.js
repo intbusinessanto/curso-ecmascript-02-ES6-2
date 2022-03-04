2eeaba115e14fdd7552bf7f282fe0b97615b01c9
intbusinessanto <intbusinessanto@hotmail.com>
2022-03-03 20:33:06 -0500

    linux server modify

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,13 +1,13 @@
 function newFunction(name, age, country) {
-  var name = name || 'oscar';
-  var age = age || 32;
-  var country = country || 'MX';
-  console.log(name, age, country);
+    var name = name || 'oscar';
+    var age = age || 32;
+    var country = country || 'MX';
+    console.log(name, age, country);
 }
 
 // es6
 function newFunction2(name = 'oscar', age = 32, country = "MX") {
-  console.log(name, age, country);
+    console.log(name, age, country);
 };
 
 newFunction2();
@@ -20,8 +20,8 @@ console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
 
-let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n"
-  + "otra frase epica que necesitamos."
+let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n" +
+    "otra frase epica que necesitamos."
 
 // es6
 let lorem2 = `otra frase epica que necesitamos
@@ -32,9 +32,9 @@ console.log(lorem);
 console.log(lorem2);
 
 let person = {
-  'name': 'oscar',
-  'age': 32,
-  'country': 'MX'
+    'name': 'oscar',
+    'age': 32,
+    'country': 'MX'
 }
 
 console.log(person.name, person.age);
@@ -50,12 +50,12 @@ let education = ['David', ...team1, ...team2];
 console.log(education);
 
 {
-  var globalVar = "Global Var";
+    var globalVar = "Global Var";
 }
 
 {
-  let globalLet = 'Global Let';
-  console.log(globalLet);
+    let globalLet = 'Global Let';
+    console.log(globalLet);
 }
 
 console.log(globalVar);
@@ -74,50 +74,50 @@ obj2 = { name, age };
 console.log(obj2);
 
 const names = [
-  { name: 'Oscar', age: 32 },
-  { name: 'Yesica', age: 27 }
+    { name: 'Oscar', age: 32 },
+    { name: 'Yesica', age: 27 }
 ]
 
-let listOfNames = names.map(function (item) {
-  console.log(item.name);
+let listOfNames = names.map(function(item) {
+    console.log(item.name);
 })
 
 let listOfNames2 = names.map(item => console.log(item.name));
 
 const listOfNames3 = (name, age, country) => {
-  ... 
+    ...
 }
 
 const listOfNames4 = name => {
-  ...
+    ...
 }
 
 const square = num => num * num;
 
 const helloPromise = () => {
-  return new Promise((resolve, reject) => {
-    if (false) {
-      resolve('Hey!');
-    } else {
-      reject('Ups!!');
-    }
-  });
+    return new Promise((resolve, reject) => {
+        if (false) {
+            resolve('Hey!');
+        } else {
+            reject('Ups!!');
+        }
+    });
 }
 
 helloPromise()
-  .then(response => console.log(response))
-  .catch(error => console.log(error));
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
 
 class calculator {
-  constructor() {
-    this.valueA = 0;
-    this.valueB = 0;
-  }
-  sum(valueA, valueB) {
-    this.valueA = valueA;
-    this.valueB = valueB;
-    return this.valueA + this.valueB;
-  }
+    constructor() {
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
 }
 
 const calc = new calculator();
@@ -128,12 +128,12 @@ import { hello } from './module';
 hello();
 
 function* helloWorld() {
-  if (true) {
-    yield 'Hello, ';
-  }
-  if (true) {
-    yield 'World';
-  }
+    if (true) {
+        yield 'Hello, ';
+    }
+    if (true) {
+        yield 'World';
+    }
 };
 
 const generatorHello = helloWorld();
